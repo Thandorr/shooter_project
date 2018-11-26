42bde856fac5ee065ff6518db143a1290ef25a45
Thandorr <jasiurkowski.kamil@gmail.com>
2018-11-26 22:16:56 +0100

    Sprawdzenie poprawności działania live renderingu poprzez aplikacje Unity Remote 5. Zakonczone pomyslnie.

---
+++ shooter_project/Assets/Scripts/Demo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour {
+
+    List<string> touchInfos = new List<string>();
+	
+	// Update is called once per frame
+	void Update () {
+        touchInfos.Clear();
+
+        for(int i=0;i<Input.touchCount;i++)
+        {
+            Touch touch = Input.GetTouch(i);
+            string tmp = "Dotknienty #" + (i + 1) + " w " + touch.position.ToString();
+            touchInfos.Add(tmp);
+        }
+		
+	}
+
+    void OnGUI()
+    {
+        foreach(string s in touchInfos)
+        {
+            GUILayout.Label(s);
+        }
+    }
+}
